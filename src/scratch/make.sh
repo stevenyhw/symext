57370c300d423b089abe31699396ef8536004a81
Steven <na>
2014-10-12 21:58:27 -0500

    add A.mf

--- src/scratch/make.sh
+++ src/scratch/make.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 myfile=$1
-
-mf ${myfile}.mf
-gftodvi ${myfile}.2602gf
-dvipdfm ${myfile}.dvi
+step=$2
+case $step in
+    1)
+    mf ${myfile}.mf
+    gftodvi ${myfile}.2602gf
+    dvipdfm ${myfile}.dvi
+    ;;
+    2)
+    mf "\mode=ljfour; mode_setup; input ${myfile}.mf"
+    ;;
+    3)
+    # pack
+    num=600
+    gftopk ${myfile}.${num}gf ${myfile}.${num}pk
+    ;;
+    4)
+    num=2602
+    gftodvi ${myfile}.${num}gf
+    dvipdfm ${myfile}.dvi
+    open ${myfile}.pdf
+    ;;
+esac
 
